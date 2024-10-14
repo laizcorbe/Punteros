3e834af74e713ba1b9767f33a1f1ea4414310bb6
laizcorbe <laizcorbe@frba.utn.edu.ar>
2024-10-14 12:08:00 -0300

    Add files via upload

--- EjercicioUno.cpp
+++ EjercicioUno.cpp
@@ -15,12 +15,18 @@ int main()
 
     p = arr;
 
-    for(int i = 0; i< 5; i++)
-    {
-        cout << "Ingrese valor N" << i << endl;
-        cin >> arr[i];
+    cout << "ingrese 5 valores" << endl;
 
-    }
+    cout << "Ingrese el valor para la posicion 1: ";
+    cin >> *p;
+    cout << "Ingrese el valor para la posicion 2: ";
+    cin >> *(p + 1);
+    cout << "Ingrese el valor para la posicion 3: ";
+    cin >> *(p + 2);
+    cout << "Ingrese el valor para la posicion 4: ";
+    cin >> *(p + 3);
+    cout << "Ingrese el valor para la posicion 5: ";
+    cin >> *(p + 4);
 
     for(int i = 0; i< 5; i++)
     {
